7ed945f7e8103fd7daf30a229ba0cc5b514fd748
Moore <n.a.moore@accenture.com>
2021-12-15 15:23:46 +0000

    Task One COMPLETE

--- src/stock_check/Initialise.java
+++ src/stock_check/Initialise.java
@@ -1,8 +1,6 @@
 package stock_check;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Initialise {
@@ -20,6 +18,11 @@ public class Initialise {
         }
     }
 
+    /**
+     *
+     * @param stocks
+     * @throws IOException
+     */
     public static void writeData(ArrayList<Stock> stocks) {
         try {
             FileWriter writer = new FileWriter(stocksFile);
@@ -37,5 +40,64 @@ public class Initialise {
             ioex.printStackTrace();
 
         }
+    }
+
+    /**
+     *
+     * @param file
+     * @return
+     * @throws
+     */
+    public ArrayList<String> readFile(File file) {
+
+        createFileIfNotExist();
+
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            FileReader reader = new FileReader(file);
+            BufferedReader br = new BufferedReader(reader);
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+            reader.close();
+        } catch (IOException ioex) {
+            System.out.println("Failed to read the file");
+            ioex.printStackTrace();
+        }
+        return lines;
+    }
+
+    /**
+     *
+     * @param stockString
+     * @return
+     */
+
+    private Stock getStockFromString(String stockString) {
+
+        Stock s;
+
+        String[] sd = stockString.split(",");
+        s = new Stock(Integer.parseInt(sd[0],
+                sd[1],
+                Integer.parseInt(sd[3]),
+                Integer.parseInt(sd[4])));
+        return s;
+    }
+
+    /**
+     *
+     * @param stockStrings
+     * @return stocks
+     */
+    public ArrayList<Stock> getStocks(ArrayList<String> stockStrings) {
+
+        ArrayList<Stock> stocks = new ArrayList<>();
+        for (String s : stockStrings) {
+            stocks.add(getStockFromString(s));
+        }
+        return stocks;
     }
 }
